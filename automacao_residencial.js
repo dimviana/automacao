1c309daa4a36ab1e4cee4bbc8e3e6a355bf10ba4
dimviana <90099585+dimviana@users.noreply.github.com>
2024-04-04 20:06:41 -0300

    Add files via upload

---
+++ automacao_residencial.js
@@ -0,0 +1,26 @@
+function AlteraRele1(){
+    var estado = document.getElementById("estado").innerHTML;
+    if(estado === "0"){
+        document.getElementById("estado").innerHTML="1";
+        document.getElementById("rele").innerHTML="<div class='rele1_ligado'></div>";
+		document.getElementById("botao").innerHTML="<a href='/?L6' class='botao'>Desliga</a>";
+    } else {
+        document.getElementById("estado").innerHTML="0";
+        document.getElementById("rele").innerHTML="<div class='rele1_desligado'></div>";
+        document.getElementById("botao").innerHTML="<a href='/?H6' class='botao'>Liga</a>";
+    }
+}
+
+function AlteraRele2(){
+    var estado_2 = document.getElementById("estado_2").innerHTML;
+    if(estado_2 === "0"){
+        document.getElementById("estado_2").innerHTML="1";
+        document.getElementById("rele_2").innerHTML="<div class='rele2_ligado'></div>";
+		document.getElementById("botao_2").innerHTML="<a href='/?G1' class='botao_2'>Desliga</a>";
+    } else {
+        document.getElementById("estado_2").innerHTML="0";
+        document.getElementById("rele_2").innerHTML="<div class='rele2_desligado'></div>";
+        document.getElementById("botao_2").innerHTML="<a href='/?G2' class='botao_2'>Liga</a>";
+    }
+}
+ 
